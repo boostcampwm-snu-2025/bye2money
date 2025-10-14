a0d092bf1a833e81714719ac4353575fc8cf4587
kreatinj <kjune1999@naver.com>
2025-10-14 20:11:53 +0900

    feat: fix lint by installing jiti and add perfectionist rule

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,20 +1,22 @@
 import js from '@eslint/js'
-import globals from 'globals'
+import perfectionist from "eslint-plugin-perfectionist";
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 export default defineConfig([
   globalIgnores(['dist']),
   {
-    files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      perfectionist.configs["recommended-natural"],
     ],
+    files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,

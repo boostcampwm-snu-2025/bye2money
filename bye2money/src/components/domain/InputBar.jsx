166549e6c13ac10d2f5813d696c725d1b9290eb3
shsha0110 <shsh0201@snu.ac.kr>
2025-10-17 10:32:12 +0900

    feat(input-bar): Implement description input with character limit

--- bye2money/src/components/domain/InputBar.jsx
+++ bye2money/src/components/domain/InputBar.jsx
@@ -114,12 +114,40 @@ function AmountInput() {
 }
 
 function DescriptionInput() {
-  return (
-    <Box sx={{ flex: 3, textAlign: "center" }}>
-      <Typography variant="body2" color="text.secondary">내용</Typography>
-      <Typography variant="body1">저녁 식사</Typography>
-    </Box>
-  );
+    const [numChar, setNumChar] = useState(0);
+    const [descriptionInput, setDescriptionInput] = useState("");
+
+    const descriptionInputHandler = (event) => {
+        const input = event.target.value;
+        if (input.length <= 32) {
+            setNumChar(input.length);
+            setDescriptionInput(input);
+        }
+    }
+
+    return (
+        <Box sx={{ flex: 2.5, textAlign: "start" }}>
+            <Box sx={{ display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
+                <Typography variant="body2" color="text.secondary">내용</Typography>
+                <Typography variant="body2" color="grey">{`${numChar}/32`}</Typography>
+            </Box>
+            <Box>
+                <input 
+                    type="text"
+                    placeholder="입력하세요"
+                    value={descriptionInput}
+                    onChange={descriptionInputHandler}
+                    style={{ 
+                        width: "100%",
+                        textAlign: "start",
+                        border: "none", 
+                        color: "black",
+                        fontSize: "1rem",
+                        background: "transparent",
+                        outline: "none"}}/>
+            </Box>
+        </Box>
+    );
 }
 
 function PaymentMethodSelect() {

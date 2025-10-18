e701987467f385cc6740780825ae96ca67406ab9
유주성 <jsryu@mp.local>
2025-10-18 20:11:29 +0900

    feat: add vite settings

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold text-blue-600">Tailwind OK</h1>
+      <button className="mt-4 rounded-xl border px-4 py-2 hover:bg-zinc-50">
+        버튼
+      </button>
+    </div>
+  );
+}

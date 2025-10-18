74e1a82bcb90be6286b0d1c4f2660e6e0ed13a82
kimnamheeee <nami200284@snu.ac.kr>
2025-10-18 23:54:34 +0900

    ✨ feat(icon): add icon component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { Icon } from "./shared/ui/Icon/Icon";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <Icon name="doc" />
+      <Icon name="calendar" />
+      <Icon name="chart" />
+      <Icon name="check" />
+      <Icon name="checkbox" />
+      <Icon name="chevron-left" />
+      <Icon name="chevron-right" />
+      <Icon name="chevron-up" />
+      <Icon name="chevron-down" />
+      <Icon name="minus" />
+      <Icon name="plus" />
+      <Icon name="closed" />
+      <Icon name="uncheckbox" />
+    </div>
+  );
 }
 
 export default App;

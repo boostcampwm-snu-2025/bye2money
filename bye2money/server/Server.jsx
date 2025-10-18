6c05dd85dd220573d44a25f40bc65d40f85415c6
shsha0110 <shsh0201@snu.ac.kr>
2025-10-18 15:50:54 +0900

    feat(server): Initialize Express server for API

---
+++ bye2money/server/Server.jsx
@@ -0,0 +1,59 @@
+import express from "express";
+import cors from "cors";
+import { DATA } from "./data.js";
+
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/paymentMethods", (req, res) => {
+    const handlePaymentMethodsRequest = (req, res) => {
+        const result = DATA["PAYMENTMETHODS"]
+        res.json({ paymentMethods: result ? result : {}, total: result ? result.length : 0});
+    }
+    handlePaymentMethodsRequest(req, res);
+});
+
+app.get("/api/transactions/:yearMonth", (req, res) => {
+    const handleSearchTransactionsRequest = (req, res) => {
+        const { yearMonth } = req.params;
+        const result = DATA["TRANSACTION"][yearMonth];
+        res.json({ transactions: result, total: result.length });
+    }
+    handleSearchTransactionsRequest(req, res);
+})
+
+
+let lastTransactionID = 0;
+app.post("/api/transactions/:yearMonth", (req, res) => {
+    const handlePostTransactionsRequest = (req, res) => {
+        const { yearMonth } = req.params;
+        const { date, type, amount, description, paymentMethod, category } = req.body;
+        const newTransaction = {
+            "id": lastTransactionID++, 
+            "date": date,
+            "type": type,
+            "amount": amount,
+            "description": description,
+            "paymentMethod": paymentMethod,
+            "category": category};
+        DATA["TRANSACTION"][yearMonth].push(newTransaction);
+    }
+    handlePostTransactionsRequest(req, res);
+})
+
+app.delete("/api/transactions/:yearMonth/:id", (req, res) => {
+    const handleDeleteTransactionsRequest = (req, res) => {
+        const { yearMonth, id } = req.params;
+        const numericID = parseInt(id, 10)
+        DATA["TRANSACTION"][yearMonth] = DATA["TRANSACTION"][yearMonth]
+                                        .filter(transcation => transcation["id"] !== numericID);
+    }
+    handleDeleteTransactionsRequest(req, res);
+})
+
+app.listen(PORT, () => {
+    console.log(`Serer running at http://localhost:${PORT}`);
+})
---
+++ bye2money/server/data.js
@@ -0,0 +1,4 @@
+export const DATA = {
+    "PAYMENTMETHODS": {},       // id, paymentMethod
+    "TRANSACTIONS": {}          // YYYY-MM: id, date, type, amount, description, paymentMethod, category
+}

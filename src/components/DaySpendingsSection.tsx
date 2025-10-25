080dacdf7c640bcee7ccb79cfa2e9999588b0774
Yeon Seok Zach Lee <winterescapade17@snu.ac.kr>
2025-10-25 14:11:50 +0900

    refactor: replace unnecessary use of ternary operator with &&.

--- src/components/DaySpendingsSection.tsx
+++ src/components/DaySpendingsSection.tsx
@@ -54,37 +54,37 @@ export const DaySpendingsSection: React.FC<DaySpendingsSectionProps> = ({
     .format(expenditureSum)
     .replaceAll("₩", "");
 
-  return validRows.length ? (
-    <div className="flex flex-col w-[846px] items-center pt-[70px] gap-[16px]">
-      <div className="flex flex-row w-full h-[16px] justify-between">
-        <h3 className="font-serif text-serif-sm">
-          {month}월 {day}일 {dayKor}
-        </h3>
-        <h3 className="font-serif text-serif-sm">
-          {incomeSum ? `수입 ${incomeSumFormatted}원  ` : ""}
-          {expenditureSum ? `지출 ${expenditureSumFormatted}원` : ""}
-        </h3>
+  return (
+    validRows.length && (
+      <div className="flex flex-col w-[846px] items-center pt-[70px] gap-[16px]">
+        <div className="flex flex-row w-full h-[16px] justify-between">
+          <h3 className="font-serif text-serif-sm">
+            {month}월 {day}일 {dayKor}
+          </h3>
+          <h3 className="font-serif text-serif-sm">
+            {incomeSum ? `수입 ${incomeSumFormatted}원  ` : ""}
+            {expenditureSum ? `지출 ${expenditureSumFormatted}원` : ""}
+          </h3>
+        </div>
+        <div className="flex flex-col w-[846px] overflow-hidden min-h-0 border-neutral-border-default border-[0.5px]">
+          {spendings
+            .filter((s) => filter.includes(s.isExpenditure))
+            .map((s) => (
+              <SpendingRow
+                key={s.id}
+                id={s.id}
+                year={s.year}
+                month={s.month}
+                day={s.day}
+                amount={s.amount}
+                isExpenditure={s.isExpenditure}
+                description={s.description}
+                paymentMethod={s.paymentMethod}
+                category={s.category}
+              />
+            ))}
+        </div>
       </div>
-      <div className="flex flex-col w-[846px] overflow-hidden min-h-0 border-neutral-border-default border-[0.5px]">
-        {spendings
-          .filter((s) => filter.includes(s.isExpenditure))
-          .map((s) => (
-            <SpendingRow
-              key={s.id}
-              id={s.id}
-              year={s.year}
-              month={s.month}
-              day={s.day}
-              amount={s.amount}
-              isExpenditure={s.isExpenditure}
-              description={s.description}
-              paymentMethod={s.paymentMethod}
-              category={s.category}
-            />
-          ))}
-      </div>
-    </div>
-  ) : (
-    <></>
+    )
   );
 };

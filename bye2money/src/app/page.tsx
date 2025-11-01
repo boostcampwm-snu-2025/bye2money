3b4044e4d919b02fa4799bb362ee9e401653008c
paul2126 <paul.mhryu@gmail.com>
2025-11-02 02:50:30 +0900

    Feat: wrap Home component with DateProvider and integrate Header component

--- bye2money/src/app/page.tsx
+++ bye2money/src/app/page.tsx
@@ -1,14 +1,23 @@
 'use client';
 
+import Header from '@/components/Header';
 import InputBar from '@/components/InputBar';
+import { DateProvider } from '@/contexts/DateContext';
 
 export default function Home() {
 
   return (
-    <div className="min-h-screen bg-gray-50">
-      <main className="py-8 px-4">
-        <InputBar  onAdd={(transaction) => console.log(transaction)} />
-      </main>
-    </div>
+    <DateProvider>
+      <div className="min-h-screen bg-gray-50">
+        <main className="py-8 px-4">
+          <div className="relative w-full">
+            <Header />
+            <div className="mt-8">
+              <InputBar onAdd={(transaction) => console.log(transaction)} />
+            </div>
+          </div>
+        </main>
+      </div>
+    </DateProvider>
   );
 }

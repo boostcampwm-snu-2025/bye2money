f6dd99e5fab40e4a62c0cba9a987af529c6c4f0a
kimnamheeee <nami200284@snu.ac.kr>
2025-11-02 15:49:58 +0900

    ✨ feat(mocks): implement MSW worker setup

---
+++ src/mocks/index.ts
@@ -0,0 +1,12 @@
+export async function initMsw() {
+  if (typeof window === "undefined") {
+    const { server } = await import("./server");
+    server.listen();
+  } else {
+    const { worker } = await import("./browser");
+    await worker.start({
+      onUnhandledRequest: "bypass",
+      serviceWorker: { url: "/mockServiceWorker.js" },
+    });
+  }
+}

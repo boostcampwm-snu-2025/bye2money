63515e7d283f1edbac16575f819921b59bbe097d
kreatinj <kjune1999@naver.com>
2025-11-02 16:19:22 +0900

    refactor(fe): CalendarView

--- fe/src/app/calendar-view/index.tsx
+++ fe/src/app/calendar-view/index.tsx
@@ -2,6 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { type Dayjs } from "dayjs";
 
 import { readTransactions } from "../../api";
+import { dailyTotals, fold, range } from "./helper";
 
 interface Props {
   date: Dayjs;
@@ -17,41 +18,21 @@ function CalendarView({ date }: Props) {
   const query = useQuery({
     queryFn: async ({ signal }) => readTransactions(month, year, signal),
     queryKey: ["transactions", { month, year }],
-    select: (data) => {
-      const dailyData = Array(daysInMonth).fill(0).map(() => ({
-        totalAmount: 0,
-        totalExpense: 0,
-        totalIncome: 0,
-      })) as {
-        totalAmount: number;
-        totalExpense: number;
-        totalIncome: number;
-      }[];
-      data.forEach((item) => {
-        const day = item.date.date() - 1;
-        dailyData[day].totalAmount += item.amount;
-        if (item.amount < 0) {
-          dailyData[day].totalExpense += item.amount;
-        } else {
-          dailyData[day].totalIncome += item.amount;
-        }
-      });
-      return {
-        dailyData,
-        monthlyData: {
-          totalAmount: data.reduce((acc, item) => acc + item.amount, 0),
-          totalExpense: data
-            .filter((item) => item.amount < 0)
-            .reduce((acc, item) => acc - item.amount, 0),
-          totalIncome: data
-            .filter((item) => item.amount > 0)
-            .reduce((acc, item) => acc + item.amount, 0),
-        }
-      };
-    },
+    select: (data) => ({
+      dailyData: dailyTotals(data, daysInMonth),
+      monthlyData: {
+        totalAmount: data.reduce((acc, item) => acc + item.amount, 0),
+        totalExpense: data
+          .filter((item) => item.amount < 0)
+          .reduce((acc, item) => acc - item.amount, 0),
+        totalIncome: data
+          .filter((item) => item.amount > 0)
+          .reduce((acc, item) => acc + item.amount, 0),
+      },
+    }),
   });
 
-  if (query.status !== 'success') return null;
+  if (query.status !== "success") return null;
 
   return (
     <>
@@ -77,17 +58,26 @@ function CalendarView({ date }: Props) {
                       <div>
                         {query.data.dailyData[i].totalIncome !== 0 && (
                           <div className="text-[14px] leading-[24px] tracking-normal font-light font-[Pretendard] text-[#79B2CA]">
-                            {query.data.dailyData[i].totalIncome.toLocaleString()}원
+                            {query.data.dailyData[
+                              i
+                            ].totalIncome.toLocaleString()}
+                            원
                           </div>
                         )}
                         {query.data.dailyData[i].totalExpense !== 0 && (
                           <div className="text-[14px] leading-[24px] tracking-normal font-light font-[Pretendard] text-[#C04646]">
-                            {query.data.dailyData[i].totalExpense.toLocaleString()}원
+                            {query.data.dailyData[
+                              i
+                            ].totalExpense.toLocaleString()}
+                            원
                           </div>
                         )}
                         {query.data.dailyData[i].totalAmount !== 0 && (
                           <div className="text-[14px] leading-[24px] tracking-normal font-light font-[Pretendard]">
-                            {query.data.dailyData[i].totalAmount.toLocaleString()}원
+                            {query.data.dailyData[
+                              i
+                            ].totalAmount.toLocaleString()}
+                            원
                           </div>
                         )}
                       </div>
@@ -108,14 +98,12 @@ function CalendarView({ date }: Props) {
             총 수입
           </span>
           <span className="text-[14px] leading-[16px] tracking-normal font-normal font-[ChosunNM]">
-            {/* TODO */}
             {query.data.monthlyData.totalIncome.toLocaleString()}원
           </span>
           <span className="text-[14px] leading-[16px] tracking-normal font-normal font-[ChosunNM]">
             총 지출
           </span>
           <span className="text-[14px] leading-[16px] tracking-normal font-normal font-[ChosunNM]">
-            {/* TODO */}
             {query.data.monthlyData.totalExpense.toLocaleString()}원
           </span>
         </div>
@@ -124,25 +112,12 @@ function CalendarView({ date }: Props) {
             총합
           </span>
           <span className="text-[14px] leading-[16px] tracking-normal font-normal font-[ChosunNM]">
-            {/* TODO */}
             {query.data.monthlyData.totalAmount.toLocaleString()}원
           </span>
         </div>
       </div>
     </>
   );
-}
-
-function fold<T>(array: T[], count: number) {
-  const result: T[][] = [];
-  for (let i = 0; i < array.length; i += count) {
-    result.push(array.slice(i, i + count));
-  }
-  return result;
-}
-
-function range(start: number, end: number) {
-  return Array.from({ length: end - start }, (_, i) => i + start);
 }
 
 export default CalendarView;
